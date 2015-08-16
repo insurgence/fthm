8db59661ca348d7707f00e61f7a249f64f02bcc9
vadim <insurgence@mail.ru>
2015-08-16 16:17:05 +0400

    Commit: Show drives

--- fthm.pl
+++ fthm.pl
@@ -4,4 +4,18 @@
 
 use strict;
 use warnings;
+use Win32::DriveInfo;
+use Win32API::File 0.08 qw( :ALL );
+
+my @drives = Win32::DriveInfo::DrivesInUse();
+my @hard_drives;
+my $N = 1;
+
+for(my $i = $[; $i <= $#drives; $i++)
+{
+	print "$N." . "$drives[$i]\n";
+	$hard_drives[$N] = $drives[$i];
+	$N++;
+}
+
 
